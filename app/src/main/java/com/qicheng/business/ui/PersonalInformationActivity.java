8aa2b36e558bd5daddadf124c369a667023d2289
jyl517hf <jyl517hf@126.com>
2015-03-12 10:47:23 +0800

    update personal information Activity

--- app/src/main/java/com/qicheng/business/ui/PersonalInformationActivity.java
+++ app/src/main/java/com/qicheng/business/ui/PersonalInformationActivity.java
@@ -51,18 +51,16 @@ public class PersonalInformationActivity extends BaseActivity {
     public void initView(LayoutInflater inflater) {
         view = inflater.inflate(R.layout.activity_personal_information, null);
         linearLayout = (LinearLayout) view.findViewById(R.id.label_scroll_root);
-          /*个人资料menu*/
-        initViewItem(inflater, R.string.personal, R.drawable.ic_personal);
-        /*我的标签menu*/
-        initViewItem(inflater, R.string.my_label, R.drawable.ic_personal);
-        /*我的相册menu*/
-        initViewItem(inflater, R.string.my_photo, R.drawable.ic_personal);
-        /*我的动态menu*/
-        initViewItem(inflater, R.string.my_activity, R.drawable.ic_personal);
-        /*筛选设置menu*/
-        initViewItem(inflater, R.string.select_setting, R.drawable.ic_fliter);
-         /*账户设置menu*/
-        initViewItem(inflater, R.string.account_setting, R.drawable.ic_account_setting);
+          /*个人头像*/
+        initPortraitItem(inflater, R.string.personal_portrait_text, R.drawable.ic_test_img);
+        /*个人昵称*/
+        initViewItem(inflater, R.string.personal_nickname_text, R.drawable.ic_personal);
+        /*性别*/
+        initViewItem(inflater, R.string.personal_gender_text, R.drawable.ic_personal);
+        /*工作*/
+        initViewItem(inflater, R.string.personal_job_text, R.drawable.ic_personal);
+        /*家乡*/
+        initViewItem(inflater, R.string.personal_home_text, R.drawable.ic_fliter);
 
     }
 
@@ -113,6 +111,52 @@ public class PersonalInformationActivity extends BaseActivity {
         linearLayout.addView(view);
     }
 
+
+    /**
+     * 初始化头像修改item
+     * @param inflater
+     * @param stringID
+     * @param resId
+     */
+   private void initPortraitItem(LayoutInflater inflater, final int stringID, int resId){
+       View view = inflater.inflate(R.layout.personal_information_portrait_tabel, null);
+       TextView viewText = (TextView) view.findViewById(R.id.table_text);
+       viewText.setText(stringID);
+
+       ImageView personalPortraitImg = (ImageView) view.findViewById(R.id.portrait);
+       personalPortraitImg.setImageResource(resId);
+        /*绑定点击事件*/
+       view.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               switch (stringID) {
+                   case R.string.personal:
+                        /*跳转到个人资料页面*/
+                       break;
+                   case R.string.my_label:
+                        /*跳转到我的标签*/
+                       getUserLabel();
+                       break;
+                   case R.string.my_photo:
+                       /*跳转到我的相册*/
+                       break;
+                   case R.string.my_activity:
+                        /*跳转到我的动态*/
+                       getUserDyn();
+                       break;
+                   case R.string.select_setting:
+                         /*跳转到筛选设置*/
+                       break;
+                   case R.string.account_setting:
+                       /*跳转到账户设置*/
+                       break;
+                   default:
+                       break;
+               }
+           }
+       });
+       linearLayout.addView(view);
+    }
     public void getUserLabel() {
         LabelLogic labelLogic = (LabelLogic) LogicFactory.self().get(LogicFactory.Type.Label);
         labelLogic.getUserLabel(createUIEventListener(new EventListener() {
@@ -137,9 +181,6 @@ public class PersonalInformationActivity extends BaseActivity {
         intent.putExtra(Const.Intent.DYN_QUERY_NAME, "我的动态");
         startActivity(intent);
     }
-
-
-
 
 
     @Override
