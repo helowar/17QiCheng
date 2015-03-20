082d706c339932e31eeb702fe339eb6be4e9d9a8
Mark Chen <chenlie.hz@gmail.com>
2015-03-20 20:13:33 +0800

    update Chat

--- app/src/main/java/com/qicheng/business/ui/MessageFragment.java
+++ app/src/main/java/com/qicheng/business/ui/MessageFragment.java
@@ -98,6 +98,7 @@ public class MessageFragment extends BaseFragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        setHasOptionsMenu(true);
     }
 
     @Override
